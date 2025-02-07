9ed9d0d3037b4242655545704a62a9a2f7730e7e
Diego <putindeergaming@gmail.com>
2025-02-07 03:35:55 -0300

    Update Events.java

--- src/main/java/me/putindeer/puebloHG/Events.java
+++ src/main/java/me/putindeer/puebloHG/Events.java
@@ -3,8 +3,14 @@ package me.putindeer.puebloHG;
 import com.destroystokyo.paper.event.block.AnvilDamagedEvent;
 import jdk.jfr.Event;
 import org.bukkit.Bukkit;
+import org.bukkit.GameMode;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.PlayerDeathEvent;
 
 public class Events implements Listener {
     private final Main plugin;
@@ -17,5 +23,27 @@ public class Events implements Listener {
     @EventHandler
     public void onAnvil(AnvilDamagedEvent e) {
         e.setCancelled(true);
+    }
+
+    @EventHandler
+    public void onArmorStandExplode(EntityDamageEvent e) {
+        if (e.getEntity().getType() != EntityType.ARMOR_STAND) return;
+        if (e.getCause() == EntityDamageEvent.DamageCause.ENTITY_EXPLOSION ||
+                e.getCause() == EntityDamageEvent.DamageCause.BLOCK_EXPLOSION) {
+            e.setCancelled(true);
+        }
+    }
+
+    @EventHandler
+    public void onArmorStandDamage(EntityDamageByEntityEvent e) {
+        if (e.getEntity().getType() != EntityType.ARMOR_STAND) return;
+        if (!(e.getDamager() instanceof Player p)) return;
+        if (p.getGameMode() != GameMode.SURVIVAL) return;
+        e.setCancelled(true);
+    }
+
+    @EventHandler
+    public void onDeath(PlayerDeathEvent e) {
+        e.setShouldDropExperience(false);
     }
 }
